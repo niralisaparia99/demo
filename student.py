ea951843d88431a891280e5e715048f6caf91da6
Nirali Saparia <niralipatel788@gmail.com>
2023-12-21 23:21:09 +0530

    add, update, delete apis

--- student.py
+++ student.py
@@ -6,7 +6,7 @@ from flask import jsonify, request
 app = Flask("__name__")
 
 
-@app.route("/list", methods=["POST"])
+@app.route("/list", methods=["POST", "GET"])
 def list_data():
     with open('studentdata.json', 'r') as file:
         student_data = json.load(file)
@@ -45,6 +45,64 @@ def student_data():
         return f"No student found with the name {name}"
     else:
         return jsonify(res_list)
+
+
+@app.route("/add-data", methods=["post"])
+def add_data():
+
+    with open("studentdata.json", "r") as file:
+        student_data= json.load(file)
+
+    data = request.get_json()
+
+    student_data["students"].append(data)
+
+    with open("studentdata.json", "w") as file:
+        json.dump(student_data, file)
+    return jsonify({"message": "Data added successfully"}), 200
+
+
+@app.route("/update-data/<string:name>", methods=["put"])
+def update_data(name):
+
+    with open("studentdata.json", "r") as file:
+        student_data = json.load(file)
+
+    students = student_data["students"]
+
+    for student in students:
+        if student["name"] == name:
+            student.update(request.get_json())  # Update the student's data
+            with open("studentdata.json", "w") as file:
+                json.dump(student_data, file)
+            return jsonify({"message": "Student data updated successfully"}), 200
+
+    return jsonify({"error": "Student not found"}), 404
+
+@app.route("/delete-data", methods=["delete"])
+
+def delete_data():
+
+    with open("studentdata.json", "r") as file:
+        student_data = json.load(file)
+
+    students = student_data["students"]
+    name = request.args.get("name", default=None)
+
+    student_to_remove = None
+
+    for student in students:
+        if student["name"] == name:
+            student_to_remove = student
+            break
+
+    if student_to_remove:
+        students.remove(student_to_remove)  # Remove the student from the list
+        with open("studentdata.json", "w") as file:
+            json.dump(student_data, file)  # Save the updated data to the JSON file
+        return jsonify({"message": f"Student {name} deleted successfully"}), 200
+    else:
+        return jsonify({"error": f"Student {name} not found"}), 404
 
 
 if __name__ == "__main__":
